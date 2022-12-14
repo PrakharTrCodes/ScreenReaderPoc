2975285403061e84db8857f01feeef1672416f49
“PrakharTrCodes” <prakhar.tripathi@appinventiv.com>
2022-12-14 15:36:29 +0530

    Publishing my branch

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,15 +1,19 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react"
-import {SafeAreaView, TouchableOpacity,Text} from 'react-native'
-const Profile=()=>{
-    const navigation = useNavigation();
-    return(
-        <SafeAreaView>
-            <Text>{'This is Profile'}</Text>
-            <TouchableOpacity onPress={()=>{
-                    navigation.goBack()
-            }}><Text>{'Back'}</Text></TouchableOpacity>
-        </SafeAreaView>
-    )
-}
-export default Profile
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {SafeAreaView, TouchableOpacity, Text} from 'react-native';
+const Profile = () => {
+  const navigation = useNavigation();
+  console.log('hello');
+  return (
+    <SafeAreaView>
+      <Text>{'This is Profile'}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        <Text>{'Back'}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+export default Profile;
